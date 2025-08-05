2bbc57e25d5dea604d3b318ae46c6b3396710c56
mayu <@em.com>
2025-08-05 16:33:16 +0900

    定数の追加

--- src/main/java/com/example/demo/Constants/Constants.java
+++ src/main/java/com/example/demo/Constants/Constants.java
@@ -1,6 +1,9 @@
 package com.example.demo.Constants;
 
 public class Constants {
+	public static final String NOT_FOUND_ITEM = "アイテムが存在しません";
+	public static final String NOT_FOUND_LENDING = "貸出情報が存在しません";
 	public static final String NOT_FOUND_ITEM_ID = "指定したアイテムIDが存在しません";
 	public static final String LENDING_ITEM = "貸出中のアイテムです";
+	
 }
