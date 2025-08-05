77a3eeb369552db59be7e49f5f7311621fb0b454
mayu <@em.com>
2025-08-05 09:28:56 +0900

    LendingController作成

---
+++ src/main/java/com/example/demo/controller/LendingController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.ErrorResponse;
+import com.example.demo.entity.Item;
+import com.example.demo.entity.Lending;
+import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.LendingRepository;
+
+@RestController
+@RequestMapping("/lending")
+public class LendingController {
+	private LendingRepository LendingRepository;
+	private ItemRepository itemRepository;
+	
+	public LendingController(LendingRepository LendingRepository, ItemRepository ItemRepository) {
+		this.LendingRepository = LendingRepository;
+	}
+	@GetMapping
+	public List<Lending> getAll(){
+		return this.LendingRepository.findAll();
+	}
+	@PostMapping
+	public ResponseEntity<Object> save(@RequestBody Lending lending) {
+		Optional<Item> item = this.itemRepository.findById(lending.getItemid());//指定したアイテムIDが備品テーブルに存在しているかチェック
+		if(item.isEmpty()) {
+			ErrorResponse errorResponse = generateErrorResponse();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		}
+		this.LendingRepository.save(lending);
+		return ResponseEntity.status(HttpStatus.OK).body(lending);
+	}
+	private ErrorResponse generateErrorResponse() {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setMessage("指定したアイテムIDが存在しません");
+		return errorResponse;
+	}
+}
