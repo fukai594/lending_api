03a27ab36b82ac10c8aeef4c46b2cbc430af2627
mayu <@em.com>
2025-08-05 16:41:38 +0900

    定数の追加：廃棄済みアイテム

--- src/main/java/com/example/demo/Constants/Constants.java
+++ src/main/java/com/example/demo/Constants/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
 	public static final String NOT_FOUND_LENDING = "貸出情報が存在しません";
 	public static final String NOT_FOUND_ITEM_ID = "指定したアイテムIDが存在しません";
 	public static final String LENDING_ITEM = "貸出中のアイテムです";
-	
+	public static final String DELETED_ITEM = "廃棄済みのアイテムです";
 }
