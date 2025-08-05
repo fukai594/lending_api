ae98ed362b624bf4e0fc9833bc9d2d0383103179
mayu <@em.com>
2025-08-05 14:47:25 +0900

    Constantsクラスの作成

---
+++ src/main/java/com/example/demo/Constants/Constants.java
@@ -0,0 +1,6 @@
+package com.example.demo.Constants;
+
+public class Constants {
+	public static final String NOT_FOUND_ITEM_ID = "指定したアイテムIDが存在しません";
+	public static final String LENDING_ITEM = "貸出中のアイテムです";
+}
